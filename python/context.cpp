3c898b5159549552f3bcc79706946868572f91e2
Anne Kuesters <a.kuesters@fz-juelich.de>
2019-05-08 14:31:54 +0200

    WIP: align gpu to conversion/exception handling

--- python/context.cpp
+++ python/context.cpp
@@ -8,7 +8,7 @@
 #include <pybind11/pybind11.h>
 
 #include "context.hpp"
-#include "exception.hpp"
+#include "conversion.hpp"
 #include "strings.hpp"
 
 #ifdef ARB_MPI_ENABLED
@@ -20,20 +20,9 @@
 
 namespace pyarb {
 
+namespace {
 auto is_int_or_minone = [](auto&& t){ return ((t==int(t) and t>=0) or t==-1); };
-
-#ifdef ARB_MPI_ENABLED
-#ifdef ARB_WITH_MPI4PY
-auto is_mpi_comm = [](auto&& t){
-    import_mpi4py();
-    return PyObject_TypeCheck(t.comm, &PyMPIComm_Type);
-};
-#else
-auto is_mpi_comm = [](auto&& t){
-    return int (t)==t;
-};
-#endif
-#endif
+}
 
 void register_contexts(pybind11::module& m) {
     using namespace std::string_literals;
@@ -45,14 +34,14 @@ void register_contexts(pybind11::module& m) {
     proc_allocation
         .def(pybind11::init<>())
         .def(pybind11::init(
-            [](int threads, opt_int gpu){
-                int gpu_id = pyarb::assert_predicate(gpu.value_or(-1), is_int_or_minone, "gpu must be None, or a non-negative integer.");
-                return arb::proc_allocation(threads, gpu_id);
+            [](int threads, pybind11::object gpu){
+                opt_int gpu_id = py2optional<int>(gpu, "gpu must be None, or a non-negative integer.", is_int_or_minone);
+                return arb::proc_allocation(threads, gpu_id.value_or(-1));
             }),
             "threads"_a=1, "gpu"_a=pybind11::none(),
              "Arguments:\n"
-             "  threads: The number of threads available locally for execution, defaults to 1.\n"
-             "  gpu:     The index of the GPU to use, defaults to None for no GPU.\n")
+             "  threads: The number of threads available locally for execution (defaults to 1).\n"
+             "  gpu:     The index of the GPU to use (defaults to None for no GPU).\n")
         .def_readwrite("threads", &arb::proc_allocation::num_threads,
             "The number of threads available locally for execution.")
         .def_readwrite("gpu_id", &arb::proc_allocation::gpu_id,
@@ -80,42 +69,43 @@ void register_contexts(pybind11::module& m) {
              "  alloc:   The computational resources to be used for the simulation.\n"
              "  c:       The MPI communicator.\n")
         .def(pybind11::init(
-            [](int threads, opt_int gpu, opt_mpi_comm mpi){
-//            [](int threads, opt_int gpu, pybind11::object mpi){
-                arb::proc_allocation alloc(threads, pyarb::assert_predicate(gpu.value_or(-1), is_int_or_minone, "gpu must be None, or a non-negative integer."));
-/*                if (mpi.is_none()) {
+//            [](int threads, opt_int gpu, opt_mpi_comm mpi){
+            [](int threads, pybind11::object gpu, pybind11::object mpi){
+                opt_int gpu_id = py2optional<int>(gpu, "gpu must be None, or a non-negative integer.", is_int_or_minone);
+                arb::proc_allocation alloc(threads, gpu_id.value_or(-1));
+                if (mpi.is_none()) {
                     return context_shim(arb::make_context(alloc));
                 }
                 auto& c = pybind11::cast<mpi_comm_shim&>(mpi);
                 return context_shim(arb::make_context(alloc, c.comm));
-*/
-#ifdef ARB_WITH_MPI4PY
-// communicator is pointer
-                auto& c = pyarb::assert_predicate(mpi.value_or(MPI_COMM_WORLD), is_mpi_comm, "mpi must be None, or an MPI communicator.");
-#else
-// communicator is integer
-                auto& c = pyarb::assert_predicate(mpi.value_or(MPI_COMM_WORLD), is_mpi_comm, "mpi must be None, or an MPI communicator.");             
-#endif
-                if (!c.comm) {
-                    return context_shim(arb::make_context(alloc));
-                }
-                else return context_shim(arb::make_context(alloc, c.comm));
+
+//#ifdef ARB_WITH_MPI4PY
+//// communicator is pointer
+//                auto& c = pyarb::assert_predicate(mpi.value_or(MPI_COMM_WORLD), is_mpi_comm, "mpi must be None, or an MPI communicator.");
+//#else
+//// communicator is integer
+//                auto& c = pyarb::assert_predicate(mpi.value_or(MPI_COMM_WORLD), is_mpi_comm, "mpi must be None, or an MPI communicator.");
+//#endif
+//                if (!c.comm) {
+//                    return context_shim(arb::make_context(alloc));
+//                }
+//                else return context_shim(arb::make_context(alloc, c.comm));
             }),
              "threads"_a=1, "gpu"_a=pybind11::none(), "mpi"_a=pybind11::none(),
              "Arguments:\n"
-             "  threads: The number of threads available locally for execution, defaults to 1.\n"
-             "  gpu:     The index of the GPU to use, defaults to None for no GPU.\n"
-             "  mpi:     The MPI communicator, defaults to None for no MPI.\n")
+             "  threads: The number of threads available locally for execution (defaults to 1).\n"
+             "  gpu:     The index of the GPU to use (defaults to None for no GPU).\n"
+             "  mpi:     The MPI communicator (defaults to None for no MPI).\n")
 #else
         .def(pybind11::init(
-            [](int threads, opt_int gpu){
-                int gpu_id = pyarb::assert_predicate(gpu.value_or(-1), is_int_or_minone, "gpu must be None, or a non-negative integer.");
-                return context_shim(arb::make_context(arb::proc_allocation(threads, gpu_id)));
+            [](int threads, pybind11::object gpu){
+                opt_int gpu_id = py2optional<int>(gpu, "gpu must be None, or a non-negative integer.", is_int_or_minone);
+                return context_shim(arb::make_context(arb::proc_allocation(threads, gpu_id.value_or(-1))));
             }),
              "threads"_a=1, "gpu"_a=pybind11::none(),
              "Arguments:\n"
-             "  threads: The number of threads available locally for execution, defaults to 1.\n"
-             "  gpu:     The index of the GPU to use, defaults to None for no GPU.\n")
+             "  threads: The number of threads available locally for execution (defaults to 1).\n"
+             "  gpu:     The index of the GPU to use (defaults to None for no GPU).\n")
 #endif
         .def_property_readonly("has_mpi", [](const context_shim& ctx){return arb::has_mpi(ctx.context);},
             "Whether the context uses MPI for distributed communication.")
